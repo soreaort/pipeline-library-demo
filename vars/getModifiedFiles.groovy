db952467dd5d8a4a36e5abe3be3760dc58d46fbf
Saul Orea <soreaort@gmail.com>
2021-04-13 13:12:11 -0500

    rollback

--- vars/getModifiedFiles.groovy
+++ vars/getModifiedFiles.groovy
@@ -1,11 +1,10 @@
 #!/usr/bin/env groovy
-import hudson.model.*
 
 
 // returns a list of changed files
 @NonCPS
 def call () {
-/*    changedFiles = []
+    changedFiles = []
     for (changeLogSet in currentBuild.changeSets) { 
         for (entry in changeLogSet.getItems()) { // for each commit in the detected changes
             for (file in entry.getAffectedFiles()) {
@@ -14,10 +13,4 @@ def call () {
         }
     }
     return changedFiles
-*/
-println(currentBuild.changeSets) // should print an empty set
-
-checkout(scm)
-
-println(currentBuild.changeSets) // should print out any changes in the current build
 }
