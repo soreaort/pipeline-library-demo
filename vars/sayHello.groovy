befa17c26ea11b26ea7ebdfa253519e585af54e8
Saul Orea <soreaort@gmail.com>
2021-04-13 17:09:23 -0500

    TEST2 from library

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String name = 'human') {
-  echo "Hello, ${name} from the library - It works just fine."
+  echo "Hello, ${name} from the library - It works just fine TEST2."
 }
 
