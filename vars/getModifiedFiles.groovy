04bbbbaeec14cddea065e6ed067a792de727930e
Saul Orea <soreaort@gmail.com>
2021-04-13 12:14:43 -0500

    Add getModifiedFiles.groovy

---
+++ vars/getModifiedFiles.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/env groovy
+
+
+// returns a list of changed files
+@NonCPS
+def call () {
+    changedFiles = []
+    for (changeLogSet in currentBuild.changeSets) { 
+        for (entry in changeLogSet.getItems()) { // for each commit in the detected changes
+            for (file in entry.getAffectedFiles()) {
+                changedFiles.add(file.getPath()) // add changed file to list
+            }
+        }
+    }
+    return changedFiles
+}
